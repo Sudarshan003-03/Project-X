fd3e5740a73c0271d81b01fd08bbf5225db6a4c8
Sudarshan003-03 <sudarshant.p798@gmail.com>
2026-02-26 11:24:30 +0530

    Removed filename

--- TriangleCheck.java
+++
@@ -1,8 +0,0 @@
-class TriangleCheck{
-    public static void main(String[] args) {
-        int a = 45,b=45,c=45;
-        String result = (a+b+c)>180?"yes":"no";
-        System.out.println(result);
-
-    }
-}
