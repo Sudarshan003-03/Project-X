beabb56427abcc8efeb39b17716c36df6d1af968
Sudarshan003-03 <sudarshant.p798@gmail.com>
2026-03-11 20:25:30 +0530

    Created

---
+++ Pattern8.java
@@ -0,0 +1,18 @@
+class Pattern8{
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+        for(int i=n-1;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ TriangleCheck.java
@@ -0,0 +1,8 @@
+class TriangleCheck{
+    public static void main(String[] args) {
+        int a = 45,b=45,c=45;
+        String result = (a+b+c)>180?"yes":"no";
+        System.out.println(result);
+
+    }
+}
