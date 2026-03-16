5b8773dc97086e3791ac1fef575966c0c252df91
Sudarshan003-03 <sudarshant.p798@gmail.com>
2026-03-16 22:50:24 +0530

    Created PalinDrome.java

---
+++ PalinDrome.java
@@ -0,0 +1,12 @@
+class Palindrome{
+    public static void main(String[] args){
+        int n = 121;
+        int temp =n,sum=0;
+        while(n!=0){
+            int a = n%10;
+            sum = sum * 10 + a;
+            n/=10;
+        }
+        System.out.println(""+(temp==sum));
+    }
+}
